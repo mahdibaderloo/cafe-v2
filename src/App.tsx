6c3d20f50e6a322264b7d40369d59ef371c50c19
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-12-28 20:11:23 +0330

    add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import AppLayout from "./AppLayout";
+import Home from "./pages/Home";
+import ShoppingCart from "./pages/ShoppingCart";
+import AboutUs from "./pages/AboutUs";
+import Login from "./pages/Login";
+
 const router = createBrowserRouter([
-  { element: <main />, children: [{ path: "/", element: "home" }] },
+  {
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/shopping-cart", element: <ShoppingCart /> },
+      { path: "/about-us", element: <AboutUs /> },
+      { path: "/login", element: <Login /> },
+    ],
+  },
 ]);
 
 function App() {
