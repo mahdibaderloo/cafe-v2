4baf9a650825eea2700ad6fe8a7e106ccc1f508f
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-12-30 10:21:36 +0330

    get categories

--- src/hooks/useCategories.ts
+++ src/hooks/useCategories.ts
@@ -1,12 +1,22 @@
 import { useMemo } from "react";
 import { useItems } from "./useItems";
+import { CATEGORIES } from "../utils/categories";
 
 export function useCategories() {
   const { data: items, isLoading, isError } = useItems();
 
   const categories = useMemo(() => {
     if (!items) return [];
-    return [...new Set(items.map((item) => item.category))];
+
+    return Object.values(
+      items.reduce((acc, item) => {
+        const category = CATEGORIES[item.category];
+        if (!category) return acc;
+
+        acc[category.label] = category;
+        return acc;
+      }, {} as Record<string, { label: string; image: string }>)
+    );
   }, [items]);
 
   return {
---
+++ src/utils/categories.ts
@@ -0,0 +1,78 @@
+export type Category = {
+  label: string;
+  image: string;
+};
+
+export const CATEGORIES: Record<string, Category> = {
+  sadRobosta: {
+    label: "قهوه گرم",
+    image: "hot-coffee.png",
+  },
+  sadArabica: {
+    label: "قهوه گرم",
+    image: "hot-coffee.png",
+  },
+  vaftad30Robosta: {
+    label: "قهوه گرم",
+    image: "hot-coffee.png",
+  },
+  vaftad30Arabica: {
+    label: "قهوه گرم",
+    image: "hot-coffee.png",
+  },
+
+  coldCoffee_100r: {
+    label: "قهوه سرد",
+    image: "cold-coffee.png",
+  },
+  coldCoffee_7030r: {
+    label: "قهوه سرد",
+    image: "cold-coffee.png",
+  },
+  coldCoffee_7030a: {
+    label: "قهوه سرد",
+    image: "cold-coffee.png",
+  },
+  coldCoffee_100a: {
+    label: "قهوه سرد",
+    image: "cold-coffee.png",
+  },
+
+  hot: {
+    label: "نوشیدنی گرم",
+    image: "hot-drink.png",
+  },
+  cold: {
+    label: "نوشیدنی سرد",
+    image: "cold-drink.png",
+  },
+
+  shake: {
+    label: "شیک",
+    image: "shake.png",
+  },
+
+  bubbleTea: {
+    label: "بابل تی",
+    image: "bubble-tea.png",
+  },
+
+  tea_1: {
+    label: "چای و دمنوش",
+    image: "tea.png",
+  },
+  tea_2: {
+    label: "چای و دمنوش",
+    image: "tea.png",
+  },
+
+  cake: {
+    label: "کیک",
+    image: "cake.png",
+  },
+
+  snack: {
+    label: "میان وعده",
+    image: "snack.png",
+  },
+};
