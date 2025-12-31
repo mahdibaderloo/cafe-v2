8d4438552b89ab457a49f0e3925ba7f166099215
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-12-31 11:42:22 +0330

    add MenuItem

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,9 +1,28 @@
 import menuIcon from "../assets/images/menu.svg";
+import homeIcon from "../assets/images/home.svg";
+import cartIcon from "../assets/images/cart.svg";
+import aboutUsIcon from "../assets/images/about-us.svg";
+import adminIcon from "../assets/images/admin.svg";
+import MenuItem from "./MenuItem";
+
+const menuItems = [
+  { id: 1, label: "صفحه اصلی", icon: homeIcon },
+  { id: 2, label: "سبد خرید", icon: cartIcon },
+  { id: 3, label: "درباره ما", icon: aboutUsIcon },
+  { id: 4, label: "پنل ادمین", icon: adminIcon },
+];
 
 export default function Menu() {
   return (
-    <div className="bg-(--green-color) w-10 h-10 flex justify-center items-center rounded-xl fixed right-4 bottom-4 shadow-[0px_1px_2px_0px_#00000073]">
-      <img src={menuIcon} alt="icon" className="w-6" />
+    <div className="fixed right-2 bottom-2 flex items-start gap-2">
+      <div className="bg-(--green-color) w-10 h-10 flex justify-center items-center rounded-xl shadow-[0px_1px_2px_0px_#00000073]">
+        <img src={menuIcon} alt="icon" className="w-6" />
+      </div>
+      <ul className="flex gap-2">
+        {menuItems.map((item) => {
+          return <MenuItem item={item} key={item.id} />;
+        })}
+      </ul>
     </div>
   );
 }
