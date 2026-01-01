96805fe9a7c4389445d60e90e174dbd625db00a7
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-01-01 10:33:13 +0330

    add Menu categories

--- src/pages/MainMenu.tsx
+++ src/pages/MainMenu.tsx
@@ -1,3 +1,21 @@
+import { Link } from "react-router-dom";
+
 export default function MainMenu() {
-  return <div></div>;
+  return (
+    <div className="w-full">
+      <main className="w-[75%]"></main>
+      <nav className="bg-[#596D6C] overflow-scroll w-[25%] flex items-center justify-center shadow-[0px_0px_6px_-1px_#000000E5] py-3">
+        <ul className="flex flex-col gap-2.5">
+          <li className="mt-0.5 bg-(--green-color) p-0.5 pb-1 rounded-xl overflow-hidden flex flex-col justify-center items-center">
+            <Link to="/" className="flex flex-col items-center gap-1">
+              <div className="bg-[#D9D9D9] rounded-[0.625rem] overflow-hidden w-12 h-12 flex justify-center items-center">
+                <img src="" alt="{item.label}" className="" />
+              </div>
+              <p className="text-[0.5rem] text-white font-medium">قهوه داغ</p>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
