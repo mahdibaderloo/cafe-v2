d0ad32b632783518dedd67fad756c940742c6e44
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-01-01 10:37:32 +0330

    add Menu show categories with DB information

--- src/pages/MainMenu.tsx
+++ src/pages/MainMenu.tsx
@@ -1,19 +1,34 @@
 import { Link } from "react-router-dom";
+import { useCategories } from "../hooks/useCategories";
+
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 
 export default function MainMenu() {
+  const { categories } = useCategories();
+
   return (
     <div className="w-full">
       <main className="w-[75%]"></main>
       <nav className="bg-[#596D6C] overflow-scroll w-[25%] flex items-center justify-center shadow-[0px_0px_6px_-1px_#000000E5] py-3">
         <ul className="flex flex-col gap-2.5">
-          <li className="mt-0.5 bg-(--green-color) p-0.5 pb-1 rounded-xl overflow-hidden flex flex-col justify-center items-center">
-            <Link to="/" className="flex flex-col items-center gap-1">
-              <div className="bg-[#D9D9D9] rounded-[0.625rem] overflow-hidden w-12 h-12 flex justify-center items-center">
-                <img src="" alt="{item.label}" className="" />
-              </div>
-              <p className="text-[0.5rem] text-white font-medium">قهوه داغ</p>
-            </Link>
-          </li>
+          {categories.map((category) => {
+            const imageUrl = `${SUPABASE_URL}/storage/v1/object/public/categories/${category.image}`;
+            return (
+              <li
+                key={category.label}
+                className="mt-0.5 bg-(--green-color) p-0.5 pb-1 rounded-xl overflow-hidden flex flex-col justify-center items-center"
+              >
+                <Link to="/" className="flex flex-col items-center gap-1">
+                  <div className="bg-[#D9D9D9] rounded-[0.625rem] overflow-hidden w-13.5 h-13.5 flex justify-center items-center">
+                    <img src={imageUrl} alt="{item.label}" className="" />
+                  </div>
+                  <p className="text-[0.48rem] text-white font-medium">
+                    {category.label}
+                  </p>
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </div>
