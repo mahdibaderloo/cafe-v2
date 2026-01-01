09b2065b14e1b162913fb9f294cf29db98b6784e
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-01-01 10:18:59 +0330

    add Menu page

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import Home from "./pages/Home";
 import ShoppingCart from "./pages/ShoppingCart";
 import AboutUs from "./pages/AboutUs";
 import Login from "./pages/Login";
+import MainMenu from "./pages/MainMenu";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
     children: [
       { path: "/", element: <Home /> },
+      { path: "/menu", element: <MainMenu /> },
       { path: "/shopping-cart", element: <ShoppingCart /> },
       { path: "/about-us", element: <AboutUs /> },
       { path: "/login", element: <Login /> },
