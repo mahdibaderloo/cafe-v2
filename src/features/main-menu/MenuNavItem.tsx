ccf8c7113adb00d72b59fcf0cd86706285e0e4a9
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-01-01 15:13:31 +0330

    select category in Menu page

--- src/features/main-menu/MenuNavItem.tsx
+++ src/features/main-menu/MenuNavItem.tsx
@@ -1,27 +1,37 @@
-import { Link } from "react-router-dom";
+import { useCategoryStore } from "../../store/categoryStore";
 
 interface NavProps {
   image: string;
   category: {
+    id: string;
     label: string;
     image?: string;
+    dbCategories: string[];
   };
 }
 
 export default function MenuNavItem({ image, category }: NavProps) {
+  const { setCategory, setLines } = useCategoryStore();
+
+  function handleSetCategory(key: string[]) {
+    if (key.length > 1) {
+      setLines(key);
+    } else {
+      setCategory(key[0]);
+      setLines([]);
+    }
+  }
+
   return (
     <li
       key={category.label}
-      className="mt-0.5 bg-(--green-color) p-0.5 pb-1 rounded-xl overflow-hidden flex flex-col justify-center items-center"
+      className="mt-0.5 bg-(--green-color) p-0.5 pb-1 rounded-xl overflow-hidden flex flex-col justify-center items-center gap-1"
+      onClick={() => handleSetCategory(category.dbCategories)}
     >
-      <Link to="/" className="flex flex-col items-center gap-1">
-        <div className="bg-[#D9D9D9] rounded-[0.625rem] overflow-hidden w-13.5 h-13.5 p-1 flex justify-center items-center">
-          <img src={image} alt="category-image" className="" />
-        </div>
-        <p className="text-[0.48rem] text-white font-medium">
-          {category.label}
-        </p>
-      </Link>
+      <div className="bg-[#D9D9D9] rounded-[0.625rem] overflow-hidden w-13.5 h-13.5 p-1 flex justify-center items-center">
+        <img src={image} alt="category-image" className="" />
+      </div>
+      <p className="text-[0.48rem] text-white font-medium">{category.label}</p>
     </li>
   );
 }
