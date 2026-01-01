fb48e1cfe5d6934b3bf881f208a5e6a753c960a8
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-01-01 10:43:36 +0330

    add MenuNavItem

--- src/features/main-menu/MainMenuNav.tsx
+++ src/features/main-menu/MainMenuNav.tsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
 import { useCategories } from "../../hooks/useCategories";
+import MenuNavItem from "./MenuNavItem";
 
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 
@@ -7,24 +7,16 @@ export default function MainMenuNav() {
   const { categories } = useCategories();
 
   return (
-    <nav className="bg-[#596D6C] overflow-scroll w-[25%] flex items-center justify-center shadow-[0px_0px_6px_-1px_#000000E5] py-3">
+    <nav className="bg-[#596D6C] overflow-scroll w-[25%] flex items-center justify-center shadow-[0px_0px_6px_-1px_#000000E5] py-2">
       <ul className="flex flex-col gap-2.5">
         {categories.map((category) => {
           const imageUrl = `${SUPABASE_URL}/storage/v1/object/public/categories/${category.image}`;
           return (
-            <li
+            <MenuNavItem
               key={category.label}
-              className="mt-0.5 bg-(--green-color) p-0.5 pb-1 rounded-xl overflow-hidden flex flex-col justify-center items-center"
-            >
-              <Link to="/" className="flex flex-col items-center gap-1">
-                <div className="bg-[#D9D9D9] rounded-[0.625rem] overflow-hidden w-13.5 h-13.5 flex justify-center items-center">
-                  <img src={imageUrl} alt="{item.label}" className="" />
-                </div>
-                <p className="text-[0.48rem] text-white font-medium">
-                  {category.label}
-                </p>
-              </Link>
-            </li>
+              image={imageUrl}
+              category={category}
+            />
           );
         })}
       </ul>
---
+++ src/features/main-menu/MenuNavItem.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+interface NavProps {
+  image: string;
+  category: {
+    label: string;
+    image?: string;
+  };
+}
+
+export default function MenuNavItem({ image, category }: NavProps) {
+  return (
+    <li
+      key={category.label}
+      className="mt-0.5 bg-(--green-color) p-0.5 pb-1 rounded-xl overflow-hidden flex flex-col justify-center items-center"
+    >
+      <Link to="/" className="flex flex-col items-center gap-1">
+        <div className="bg-[#D9D9D9] rounded-[0.625rem] overflow-hidden w-13.5 h-13.5 flex justify-center items-center">
+          <img src={image} alt="{item.label}" className="" />
+        </div>
+        <p className="text-[0.48rem] text-white font-medium">
+          {category.label}
+        </p>
+      </Link>
+    </li>
+  );
+}
