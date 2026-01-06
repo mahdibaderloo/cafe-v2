769e8793dc06144b3b60e575c61fa01850280fb3
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-01-06 21:40:43 +0330

    set count for items

--- src/pages/ShoppingCart.tsx
+++ src/pages/ShoppingCart.tsx
@@ -2,8 +2,23 @@ import backButton from "../assets/images/back.svg";
 import itemImg from "../assets/images/item.png";
 import plusIcon from "../assets/images/plus.svg";
 import minusIcon from "../assets/images/minus.svg";
+import { useState } from "react";
 
 export default function ShoppingCart() {
+  const [count, setCount] = useState(1);
+
+  function handleIncreaseCount() {
+    if (count < 5) {
+      setCount((c) => (c = c + 1));
+    }
+  }
+
+  function handleDecreaseCount() {
+    if (count > 1) {
+      setCount((c) => (c = c - 1));
+    }
+  }
+
   return (
     <div className="w-full h-screen overflow-hidden bg-[linear-gradient(180deg,#503D32_0%,#738E7F_52.4%)]">
       <header className="w-full h-fit flex justify-between items-center p-4">
@@ -15,13 +30,13 @@ export default function ShoppingCart() {
       </header>
       <main className="w-full h-fit p-4 mt-8">
         <ul className="flex flex-col gap-2">
-          <li className="bg-[#4C3D34] rounded-xl shadow-[0px_3px_4.6px_0px_#00000066] w-full h-18 overflow-hidden p-1 flex">
-            <div className="bg-[#566C5F] rounded-xl shadow-[1px_2px_5px_0px_#00000040] w-15 h-full flex justify-center items-center">
-              <img src={itemImg} alt="item-image" className="w-14" />
+          <li className="bg-[#4C3D34] rounded-xl shadow-[0px_3px_4.6px_0px_#00000066] w-full h-19 overflow-hidden p-1 flex">
+            <div className="bg-[#566C5F] rounded-xl shadow-[1px_2px_5px_0px_#00000040] w-24.5 h-full flex justify-center items-center">
+              <img src={itemImg} alt="item-image" className="w-18" />
             </div>
 
-            <div className="text-white font-medium w-full flex flex-col justify-center gap-2 mr-4">
-              <p className="text-[0.9rem]">لیموناد</p>
+            <div className="text-white font-medium w-full flex flex-col justify-center gap-3 mr-2">
+              <p className="text-[0.8rem] mt-2">لیموناد</p>
 
               <div className="flex items-center justify-between">
                 <p className="text-[0.8rem]">150,000</p>
@@ -30,14 +45,16 @@ export default function ShoppingCart() {
                     src={plusIcon}
                     alt="icon"
                     className="bg-[#503D32] w-5 h-full p-1 rounded-[0.4rem]"
+                    onClick={handleIncreaseCount}
                   />
                   <span className="text-[#503D32] font-semibold text-sm">
-                    1
+                    {count}
                   </span>
                   <img
                     src={minusIcon}
                     alt="icon"
                     className="bg-[#4B4542] w-5 h-full p-1 rounded-[0.4rem]"
+                    onClick={handleDecreaseCount}
                   />
                 </div>
               </div>
