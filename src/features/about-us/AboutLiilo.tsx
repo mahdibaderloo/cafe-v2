5fbe3a9036a374cc6342a845a2b5c0a6ad44c1ee
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-02-04 21:00:54 +0330

    AboutLiilo

--- src/features/about-us/AboutLiilo.tsx
+++ src/features/about-us/AboutLiilo.tsx
@@ -1,3 +1,13 @@
+import liiloLogo from "../../assets/images/lillo-logo.png";
+
 export default function AboutLiilo() {
-  return <div>کافه لیلو</div>;
+  return (
+    <section className="fixed w-full flex justify-center top-0 left-1/2 -translate-x-1/2">
+      <img src={liiloLogo} alt="logo" />
+      <div className="w-full absolute top-1/2 left-1/2 -translate-x-1/2 flex flex-col justify-center items-center text-white text-[0.7rem] font-semibold">
+        <p>کافه لیلو؛</p>
+        <p>هر فنجان، یک سکوت خوش طعم</p>
+      </div>
+    </section>
+  );
 }
