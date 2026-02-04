bb2664a2e66fae7fd0e01c31f36f10cc0a4c37df
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-02-04 11:28:51 +0330

    fix menu navbar bug

--- src/features/main-menu/MenuNavItem.tsx
+++ src/features/main-menu/MenuNavItem.tsx
@@ -25,10 +25,10 @@ export default function MenuNavItem({ image, category }: NavProps) {
   return (
     <li
       key={category.label}
-      className="mt-0.5 bg-(--green-color) p-0.5 pb-1 rounded-xl overflow-hidden flex flex-col justify-center items-center gap-1"
+      className="w-full mt-0.5 bg-(--green-color) p-0.5 pb-1 rounded-xl overflow-hidden flex flex-col justify-center items-center gap-1"
       onClick={() => handleSetCategory(category.dbCategories)}
     >
-      <div className="bg-[#D9D9D9] rounded-[0.625rem] overflow-hidden w-13.5 h-13.5 p-1 flex justify-center items-center">
+      <div className="bg-[#D9D9D9] rounded-[0.625rem] overflow-hidden w-full h-fit p-1 flex justify-center items-center">
         <img src={image} alt="category-image" className="" />
       </div>
       <p className="text-[0.48rem] text-white font-medium">{category.label}</p>
