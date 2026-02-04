d5efc539a761200bed16a57d487e7ac85426efde
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-02-04 20:30:09 +0330

    add ContactUsItem

--- src/features/about-us/ContactUs.tsx
+++ src/features/about-us/ContactUs.tsx
@@ -1,6 +1,7 @@
 import phoneIcon from "../../assets/images/phone.svg";
 import locationIcon from "../../assets/images/location.svg";
 import instagramIcon from "../../assets/images/instagram.svg";
+import ContactUsItem from "./ContactUsItem";
 
 interface Item {
   id: number;
@@ -14,9 +15,9 @@ type Items = Item[];
 const items: Items = [
   {
     id: 1,
-    bg: "bg-[linear-gradient(180deg,rgba(17,51,85,0.87)_0%,rgba(126,223,178,0.93)_50%,#4FB286_100%)]",
-    icon: phoneIcon,
-    text: "تلفن",
+    bg: "bg-[linear-gradient(180deg,#833AB4_0%,#FD1D1D_50%,#FCB045_100%)]",
+    icon: instagramIcon,
+    text: "اینستاگرام",
   },
   {
     id: 2,
@@ -26,23 +27,20 @@ const items: Items = [
   },
   {
     id: 3,
-    bg: "bg-[linear-gradient(180deg,#833AB4_0%,#FD1D1D_50%,#FCB045_100%)]",
-    icon: instagramIcon,
-    text: "اینستاگرام",
+    bg: "bg-[linear-gradient(180deg,rgba(17,51,85,0.87)_0%,rgba(126,223,178,0.93)_50%,#4FB286_100%)]",
+    icon: phoneIcon,
+    text: "تلفن",
   },
 ];
 
 export default function ContactUs() {
   return (
     <div>
-      <p className="text-white text-sm">ارتباط با ما</p>
-      <ul className="flex items-center justify-center gap-4">
-        <li className="flex flex-col items-center">
-          <div className="border-3 border-[#4C3D34] bg-[linear-gradient(180deg,rgba(17,51,85,0.87)_0%,rgba(126,223,178,0.93)_50%,#4FB286_100%)] shadow-[0px_3px_4.6px_0px_#00000066] rounded-xl p-2">
-            <img src={phoneIcon} alt="icon" />
-          </div>
-          <p className="text-white text-[0.75rem] font-light mt-1">تلفن</p>
-        </li>
+      <p className="text-white text-sm mr-2.5">ارتباط با ما</p>
+      <ul className="flex items-center justify-center gap-2 mt-4">
+        {items.map((item) => (
+          <ContactUsItem key={item.id} item={item} />
+        ))}
       </ul>
     </div>
   );
---
+++ src/features/about-us/ContactUsItem.tsx
@@ -0,0 +1,25 @@
+interface Item {
+  id: number;
+  bg: string;
+  icon: string;
+  text: string;
+}
+
+type ItemProp = { item: Item };
+
+export default function ContactUsItem({ item }: ItemProp) {
+  return (
+    <li className="flex flex-col items-center w-[30%]">
+      <div
+        className={`w-15.5 h-15.5 flex items-center justify-center border-3 border-[#4C3D34] ${item.bg} shadow-[0px_3px_4.6px_0px_#00000066] rounded-xl`}
+      >
+        <img
+          src={item.icon}
+          alt="icon"
+          className={`${item.id === 2 ? "w-7" : "w-9"}`}
+        />
+      </div>
+      <p className="text-white text-[0.75rem] font-light mt-1.5">{item.text}</p>
+    </li>
+  );
+}
