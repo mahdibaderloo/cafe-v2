e933248a7c5766dee73af6fa207527865ae51da0
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-02-04 21:06:12 +0330

    Copyright text

--- src/features/about-us/CopyRight.tsx
+++ src/features/about-us/CopyRight.tsx
@@ -1,3 +1,10 @@
 export default function CopyRight() {
-  return <div></div>;
+  return (
+    <section>
+      <div className="flex gap-0.5 text-[0.5rem] font-medium text-white">
+        <span>&#9400;</span>
+        <p>تمامی حقوق این وبسایت متعلق به کافه لیلو است.</p>
+      </div>
+    </section>
+  );
 }
