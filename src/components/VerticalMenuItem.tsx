b76a3add3c79deb31060bd41aeb37a40d03001dc
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-02-16 22:15:44 +0330

    VerticalMenu (tablet size)

--- src/components/VerticalMenuItem.tsx
+++ src/components/VerticalMenuItem.tsx
@@ -12,16 +12,18 @@ interface Item {
 
 export default function VerticalMenuItem({ item, onToggleMenu }: Item) {
   return (
-    <li className="mt-0.5 w-10.5" onClick={onToggleMenu}>
+    <li className="mt-0.5 w-10.5 sm:w-12" onClick={onToggleMenu}>
       <Link to={item.url} className="flex flex-col items-center gap-1">
-        <div className="bg-[#596D6C] p-2 rounded-xl w-8 h-8 shadow-[0px_1px_2px_0px_#00000073] flex items-center justify-center">
+        <div className="bg-[#596D6C] p-2 rounded-xl w-8 h-8 sm:w-10 sm:h-10 shadow-[0px_1px_2px_0px_#00000073] flex items-center justify-center">
           <img
             src={item.icon}
             alt={item.label}
-            className={item.id === 4 ? "w-4.5" : "w-5"}
+            className={item.id === 4 ? "w-4.5 sm:w-5" : "w-5 sm:w-5.5"}
           />
         </div>
-        <p className="text-[0.5rem] text-white font-medium">{item.label}</p>
+        <p className="text-[0.5rem] sm:text-[0.55rem] text-white font-medium">
+          {item.label}
+        </p>
       </Link>
     </li>
   );
