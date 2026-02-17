a62a48fda2ee3236217f7fe297d5d311e258db36
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-02-17 19:49:16 +0330

    EmptyCart (tablet size)

--- src/features/cart/EmptyCart.tsx
+++ src/features/cart/EmptyCart.tsx
@@ -1,7 +1,7 @@
 export default function EmptyCart() {
   return (
     <div className="flex h-30 justify-center items-end">
-      <p className="text-[rgba(186,186,186,1)] font-semibold text-sm">
+      <p className="text-[rgba(186,186,186,1)] font-semibold text-sm sm:text-lg">
         سبد خرید شما خالی است!
       </p>
     </div>
