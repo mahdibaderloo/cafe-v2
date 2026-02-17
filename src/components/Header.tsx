26cab0d5520b2037c6f7eecd246943e2800b7678
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-02-17 19:48:21 +0330

    Header (tablet size)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,12 +9,12 @@ export default function Header({ text }: Text) {
   return (
     <header className="w-full h-fit flex justify-between items-center p-4">
       <div className="w-[8%]" />
-      <h3 className="text-white font-bold">{text}</h3>
+      <h3 className="text-white sm:text-xl font-bold">{text}</h3>
       <div
-        className="bg-[#14512F] rounded-xl h-7.5 w-7.5 flex justify-center items-center"
+        className="bg-[#14512F] rounded-xl h-7.5 w-7.5 sm:h-9 sm:w-9 flex justify-center items-center"
         onClick={() => navigate(-1)}
       >
-        <img src={backButton} alt="icon" />
+        <img src={backButton} alt="icon" className="sm:w-5" />
       </div>
     </header>
   );
