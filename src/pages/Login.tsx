06b5f83520173b6ad86ce72a9c13645b36ba2a83
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-02-23 20:46:10 +0330

    login form validation

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,12 +2,34 @@ import coffeeImage from "../assets/images/login-pic.png";
 import eyeOpenIcon from "../assets/images/eye-open.svg";
 import eyeCloseIcon from "../assets/images/eye-close.svg";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+interface FormValues {
+  email: string;
+  password: string;
+}
 
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@(gmail|yahoo|google|outlook)\.com$/i;
 
   function handleShowPassword() {
     setShowPassword((t) => (t === false ? true : false));
+  }
+
+  function onSubmitLogin(data: FormValues) {
+    console.log(data);
   }
 
   return (
@@ -15,7 +37,7 @@ export default function Login() {
       <div className="flex lg:w-[80%] xl:w-200 h-80 xl:h-100 bg-white rounded-3xl overflow-hidden">
         <section className="lg:w-[55%] xl:w-[62%] flex flex-col items-center pt-8 xl:pt-12">
           <h3 className="font-bold text-xl xl:text-3xl">ورود</h3>
-          <form>
+          <form onSubmit={handleSubmit(onSubmitLogin)}>
             <div className="flex flex-col gap-1 xl:gap-2 mt-8">
               <label
                 htmlFor="email"
@@ -24,11 +46,21 @@ export default function Login() {
                 ایمیل
               </label>
               <input
-                type="text"
+                type="email"
                 id="email"
+                {...register("email", {
+                  required: "وارد کردن ایمیل الزامی است",
+                  pattern: {
+                    value: emailRegex,
+                    message: "فرمت ایمیل وارد شده صحیح نیست",
+                  },
+                })}
                 className="bg-[#F2F2F2] w-45 xl:w-50 h-7 xl:h-9 border-3 border-[#748F80] rounded-lg outline-none py-0.5 text-[0.6rem] xl:text-[0.65rem] pl-1 font-semibold font-sans"
                 dir="ltr"
               />
+              <p className="text-red-600 text-[0.6rem]">
+                {errors.email?.message}
+              </p>
             </div>
             <div className="flex flex-col gap-1 xl:gap-2 mt-4 xl:mt-6">
               <label
@@ -49,11 +81,21 @@ export default function Login() {
                 <input
                   type={showPassword ? "text" : "password"}
                   id="password"
+                  {...register("password", {
+                    required: "رمز عبور را وارد کنید",
+                    minLength: {
+                      value: 8,
+                      message: "رمز عبور باید حداقل ۸ کاراکتر باشد",
+                    },
+                  })}
                   className={`w-full outline-none py-0.5 ${showPassword ? "text-[0.6rem] xl:text-[0.65rem] font-semibold" : "text-[0.8rem] xl:text-[1rem] font-semibold"} tracking-wider pl-1 font-sans`}
                   maxLength={12}
                   dir="ltr"
                 />
               </div>
+              <p className="text-red-600 text-[0.6rem]">
+                {errors.password?.message}
+              </p>
             </div>
             <div className="w-full flex justify-center">
               <button className="bg-[#485158] text-[0.9rem] xl:text-[1rem] font-semibold text-white w-30 h-9 xl:w-32 xl:h-10 rounded-lg mt-8 cursor-pointer">
