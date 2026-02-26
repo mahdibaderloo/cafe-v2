029288ade10fc34151ad9eeef8608f28cfde0f5a
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-02-26 22:21:59 +0330

    Dashboard main color

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,11 @@
+import DashboardHeader from "../features/dashboard/DashboardHeader";
+
 export default function Dashboard() {
-  return <div>Panel</div>;
+  return (
+    <div>
+      <main className="bg-[#C8D2DA]">
+        <DashboardHeader title="داشبورد" />
+      </main>
+    </div>
+  );
 }
