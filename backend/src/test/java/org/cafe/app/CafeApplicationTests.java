be7ed4c929ef34d4f59526483d9769c470e2d48c
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-07-02 20:20:15 +0330

    add backend

---
+++ backend/src/test/java/org/cafe/app/CafeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cafe.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
