c1837c4be559506694bf99d1d2fa11f2e9fc9bc4
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-07-03 10:38:16 +0330

    add User entity

---
+++ backend/src/main/java/org/cafe/app/entity/User.java
@@ -0,0 +1,17 @@
+package org.cafe.app.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private Role role;
+    private String image;
+    private String email;
+}
