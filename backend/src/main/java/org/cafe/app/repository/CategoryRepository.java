b7b81ae994c0e57e74567d5d14a6320cbddabee0
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-07-03 11:19:20 +0330

    add CategoryRepository

---
+++ backend/src/main/java/org/cafe/app/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package org.cafe.app.repository;
+
+import org.cafe.app.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
