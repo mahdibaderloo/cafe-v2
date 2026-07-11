e1ecc612a24f7ca0228250f85455e4a247fa3588
Mahdi Baderloo <mahdi.baderloo.230@gmail.com>
2026-07-11 17:22:24 +0330

    add JwtService

---
+++ backend/src/main/java/org/cafe/app/security/JwtService.java
@@ -0,0 +1,34 @@
+package org.cafe.app.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("cafeliiloadmin1381liilo1381pat0mat")
+    private String jwtSecret;
+
+    @Value("86400000")
+    private Long expirationTime;
+
+    public String generateToken (String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() * expirationTime))
+                .signWith(key())
+                .compact();
+    }
+
+    private SecretKey key () {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+}
