523a87e0178737869da2edd41d4afc9dbba027ee
Mahdi Baderloo <mahdi.baderloo.230@gmail.com>
2026-07-12 09:59:58 +0330

    add generateTokenFromUsername

--- backend/src/main/java/org/cafe/app/security/JwtService.java
+++ backend/src/main/java/org/cafe/app/security/JwtService.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -18,7 +19,16 @@ public class JwtService {
     @Value("86400000")
     private Long expirationTime;
 
-    public String generateToken (String username) {
+    public String generateToken (UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() * expirationTime))
+                .signWith(key())
+                .compact();
+    }
+
+    public String generateTokenFromUsername (String username) {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
