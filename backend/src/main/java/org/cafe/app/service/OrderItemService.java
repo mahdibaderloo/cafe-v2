842d9a85c816ec264f300da44782ddc776b8c714
Mahdi Baderloo <mahdi.baderloo.230@gmail.com>
2026-07-13 16:15:06 +0330

    OrderItemService

--- backend/src/main/java/org/cafe/app/service/OrderItemService.java
+++ backend/src/main/java/org/cafe/app/service/OrderItemService.java
@@ -1,7 +1,42 @@
 package org.cafe.app.service;
 
+import lombok.RequiredArgsConstructor;
+import org.cafe.app.dto.OrderItemResponseDto;
+import org.cafe.app.dto.OrderResponseDto;
+import org.cafe.app.entity.Order;
+import org.cafe.app.entity.OrderItem;
+import org.cafe.app.repository.OrderItemRepository;
+import org.cafe.app.repository.OrderRepository;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class OrderItemService {
+
+    private final OrderItemRepository orderItemRepository;
+
+    public List<OrderItemResponseDto> getOrderItems() {
+        List<OrderItem> orderItems = orderItemRepository.findAll();
+
+        return orderItems.stream()
+                .map(this::convertToItemDto)
+                .collect(Collectors.toList());
+    }
+
+    public OrderItemResponseDto convertToItemDto(OrderItem orderItem) {
+        return OrderItemResponseDto.builder()
+                .id(orderItem.getId())
+                .itemId(orderItem.getItem().getId())
+                .itemName(orderItem.getItem().getProductName())
+                .itemDescription(orderItem.getItem().getDescription())
+                .count(orderItem.getCount())
+                .price(orderItem.getPrice())
+                .subtotal(orderItem.getPrice().multiply(BigDecimal.valueOf(orderItem.getCount())))
+                .build();
+    }
+
 }
--- backend/src/main/java/org/cafe/app/service/OrderService.java
+++ backend/src/main/java/org/cafe/app/service/OrderService.java
@@ -1,13 +1,41 @@
 package org.cafe.app.service;
 
 import lombok.RequiredArgsConstructor;
+import org.cafe.app.dto.OrderResponseDto;
+import org.cafe.app.entity.Order;
 import org.cafe.app.repository.OrderRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final OrderItemService orderItemService;
 
+    public List<OrderResponseDto> getAllOrders() {
+        List<Order> orders = orderRepository.findAll();
+
+        return orders.stream()
+                .map(this::OrderToDto)
+                .collect(Collectors.toList());
+    }
+
+    private OrderResponseDto OrderToDto(Order order) {
+        return OrderResponseDto.builder()
+                .id(order.getId())
+                .createdAt(order.getCreatedAt())
+                .totalPrice(order.getTotalPrice())
+                .username(order.getUsername())
+                .phoneNumber(order.getPhoneNumber())
+                .takeAway(order.isTakeAway())
+                .description(order.getDescription())
+                .items(order.getItems().stream()
+                        .map(orderItemService::convertToItemDto)
+                        .collect(Collectors.toList()))
+                .build();
+    }
 }
