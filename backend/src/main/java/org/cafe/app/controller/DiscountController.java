7f68879608dc9924db192bd589a73667318c7a24
Mahdi Baderloo <mahdi.baderloo.230@gmail.com>
2026-07-13 17:21:05 +0330

    add DiscountType

---
+++ backend/src/main/java/org/cafe/app/controller/DiscountController.java
@@ -0,0 +1,13 @@
+package org.cafe.app.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/discount")
+@CrossOrigin(origins = "http://localhost:5173")
+@RequiredArgsConstructor
+public class DiscountController {
+}
---
+++ backend/src/main/java/org/cafe/app/enums/DiscountType.java
@@ -0,0 +1,6 @@
+package org.cafe.app.enums;
+
+public enum DiscountType {
+    PERCENTAGE,
+    FIXED_AMOUNT
+}
