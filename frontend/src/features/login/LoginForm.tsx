4397bfc5c14d36d2e7d30cd3a10fddf7cc7f4c5a
Mahdi <mahdi.baderloo.230@gmail.com>
2026-07-17 16:39:41 +0330

    edit Login styles

--- frontend/src/features/login/LoginForm.tsx
+++ frontend/src/features/login/LoginForm.tsx
@@ -25,7 +25,7 @@ export default function LoginForm() {
   const emailRegex = /^[a-zA-Z0-9._%+-]+@(gmail|yahoo|google|outlook)\.com$/i;
 
   function handleShowPassword() {
-    setShowPassword((t) => (t === false ? true : false));
+    setShowPassword((t) => (!t));
   }
 
   function onSubmitLogin(data: FormValues) {
@@ -36,11 +36,11 @@ export default function LoginForm() {
     });
   }
   return (
-    <section className="lg:w-[55%] xl:w-[62%] flex flex-col items-center pt-8 xl:pt-12">
-      <h3 className="font-bold text-xl xl:text-3xl">پنل ادمین</h3>
+    <section className="lg:w-[60%] xl:w-[62%] flex flex-col items-center pt-8 xl:pt-12">
+      <h3 className="font-bold text-2xl xl:text-3xl">پنل ادمین</h3>
       <form onSubmit={handleSubmit(onSubmitLogin)}>
-        <div className="flex flex-col gap-1 xl:gap-2 mt-8">
-          <label htmlFor="email" className="text-sm font-semibold">
+        <div className="flex flex-col gap-1 xl:gap-2 mt-10">
+          <label htmlFor="email" className="text-md xl:text-xl font-semibold">
             ایمیل
           </label>
           <input
@@ -53,17 +53,17 @@ export default function LoginForm() {
                 message: "فرمت ایمیل وارد شده صحیح نیست",
               },
             })}
-            className="bg-[#F2F2F2] w-45 xl:w-50 h-7 xl:h-9 border-3 border-[#748F80] rounded-lg outline-none py-0.5 text-[0.6rem] xl:text-[0.65rem] pl-1 font-semibold font-sans"
+            className="bg-[#F2F2F2] w-60 xl:w-80 h-12 xl:h-16 border-3 border-[#748F80] rounded-lg outline-none mt-2 py-0.5 text-md xl:text-lg xl:px-2 font-semibold font-sans"
             dir="ltr"
           />
-          <p className="text-red-600 text-[0.6rem]">{errors.email?.message}</p>
+          <p className="text-red-600 text-xs xl:text-[1rem]">{errors.email?.message}</p>
         </div>
-        <div className="flex flex-col gap-1 xl:gap-2 mt-4 xl:mt-6">
-          <label htmlFor="password" className="text-sm font-semibold">
+        <div className="flex flex-col gap-1 xl:gap-2 mt-6">
+          <label htmlFor="password" className="text-md  xl:text-xl font-semibold">
             رمز عبور
           </label>
 
-          <div className="bg-[#F2F2F2] flex items-center w-45 xl:w-50 h-7 xl:h-9 border-3 border-[#748F80] rounded-lg">
+          <div className="bg-[#F2F2F2] flex items-center w-60 xl:w-80 h-12 xl:h-16 border-3 border-[#748F80] rounded-lg mt-2">
             <img
               src={showPassword ? eyeOpenIcon : eyeCloseIcon}
               alt="eye-icon"
@@ -81,19 +81,19 @@ export default function LoginForm() {
                   message: "رمز عبور باید حداقل ۸ کاراکتر باشد",
                 },
               })}
-              className={`w-full outline-none py-0.5 ${showPassword ? "text-[0.6rem] xl:text-[0.65rem] font-semibold" : "text-[0.8rem] xl:text-[1rem] font-semibold"} tracking-wider pl-1 font-sans`}
+              className={`w-full outline-none py-0.5 ${showPassword ? "text-md xl:text-lg font-semibold" : "text-md xl:text-lg font-semibold"} tracking-wider px-1 xl:px-2 font-sans`}
               maxLength={12}
               dir="ltr"
             />
           </div>
-          <p className="text-red-600 text-[0.6rem]">
+          <p className="text-red-600 text-xs xl:text-[1rem]">
             {errors.password?.message}
           </p>
         </div>
         <div className="w-full flex justify-center">
           <button
             disabled={isPending}
-            className={`${isPending ? "bg-[#9d9e9e] text-[0.7rem] xl:text-[0.8rem]" : "bg-[#485158] text-[0.9rem] xl:text-[1rem]"} font-semibold text-white w-30 h-9 xl:w-32 xl:h-10 rounded-lg mt-8 cursor-pointer`}
+            className={`${isPending ? "bg-[#9d9e9e] text-lg xl:text-xl" : "bg-[#485158] text-lg xl:text-xl"} font-semibold text-white w-60 h-12 xl:w-80 xl:h-16 rounded-lg mt-10 lg:mt-18 cursor-pointer`}
           >
             {isPending ? "بررسی اطلاعات..." : "ورود"}
           </button>
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -7,19 +7,21 @@ import { Link } from "react-router-dom";
 export default function Login() {
   return (
     <div className="w-full h-screen bg-[#485158] flex flex-col justify-center items-center">
-      <Link
-        to="/"
-        className="pr-152 flex items-center gap-2 text-white text-sm lg:cursor-pointer pb-2"
-      >
-        <p>بازگشت</p>
-        <img src={backIcon} alt="" />
-      </Link>
-      <div className="flex lg:w-[80%] xl:w-200 h-80 xl:h-100 bg-white rounded-3xl overflow-hidden">
-        <LoginForm />
-        <section className="lg:w-[45%] xl:w-[38%]">
-          <img src={coffeeImage} alt="image" className="" />
-        </section>
-      </div>
+     <div className="lg:w-[85%] xl:w-260 h-120 xl:h-160 flex flex-col justify-center items-center">
+         <Link
+             to="/"
+             className="self-end flex items-center gap-2 text-white text-lg lg:cursor-pointer pb-6"
+         >
+             <p>بازگشت</p>
+             <img src={backIcon} alt="" />
+         </Link>
+         <div className="flex lg:w-full h-full bg-white rounded-3xl overflow-hidden">
+             <LoginForm />
+             <section className="lg:w-[40%] xl:w-[38%]">
+                 <img src={coffeeImage} alt="image" className="" />
+             </section>
+         </div>
+     </div>
     </div>
   );
 }
